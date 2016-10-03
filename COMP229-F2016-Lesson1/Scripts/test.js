44e45d20650a3dc8a99aae64ee9a30fe21462e09
xuemeiwang <xwang231@my.centennialcollege.ca>
2016-10-03 13:19:02 -0400

    updated test.js

--- COMP229-F2016-Lesson1/Scripts/test.js
+++ COMP229-F2016-Lesson1/Scripts/test.js
@@ -1,1 +1,11 @@
-﻿select * from books;
+﻿No. 7
+select last, first
+from criminals
+where criminal_id in
+(select criminal_id from crimes where crime_id = 10089);
+
+No. 8
+select * from criminals
+where criminal_id in (select distinct criminal_id from sentences
+where type = 'P' and start_date is not null
+and end_date is not null);
